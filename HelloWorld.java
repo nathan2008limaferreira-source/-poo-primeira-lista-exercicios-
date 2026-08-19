dbafafbb56f9cf15091e8506c3eafe56b7f3afbb
Nathan Henrique <nathan2008limaferreira@gmail.com>
2026-08-19 20:15:45 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author Aluno
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nathan Henrique Lima Ferreira - Meu Primeiro Código Java!");
+    }
+}
